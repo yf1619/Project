b11a19cc39869b7ad0305620a6f04a392b050143
yf1619 <59800322+yf1619@users.noreply.github.com>
2022-06-17 15:25:53 +0100

    Websocket_Solution

--- Control/HTTP/HTTP/src/main.cpp
+++ Control/HTTP/HTTP/src/main.cpp
@@ -1,31 +1,23 @@
+
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <Arduino_JSON.h>
+#include <Arduino.h>
+const char* ssid = "YIWEI_Laptop";
+const char* password = "1234567890";
 
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+//Your Domain name with URL path or IP address with path
+String serverName = "http://146.169.174.193:8000";
 
-// Your Domain name with URL path or IP address with path
-String openWeatherMapApiKey = "REPLACE_WITH_YOUR_OPEN_WEATHER_MAP_API_KEY";
-// Example:
-//String openWeatherMapApiKey = "bd939aa3d23ff33d3c8f5dd1dd435";
-
-// Replace with your country code and city
-String city = "Porto";
-String countryCode = "PT";
-
-// THE DEFAULT TIMER IS SET TO 10 SECONDS FOR TESTING PURPOSES
-// For a final application, check the API call limits per hour/minute to avoid getting blocked/banned
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastTime = 0;
 // Timer set to 10 minutes (600000)
 //unsigned long timerDelay = 600000;
-// Set timer to 10 seconds (10000)
-unsigned long timerDelay = 10000;
-
-String jsonBuffer;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); 
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
@@ -37,67 +29,40 @@ void setup() {
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
  
-  Serial.println("Timer set to 10 seconds (timerDelay variable), it will take 10 seconds before publishing the first reading.");
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
 void loop() {
-  // Send an HTTP GET request
+  //Send an HTTP POST request every 10 minutes
   if ((millis() - lastTime) > timerDelay) {
-    // Check WiFi connection status
+    //Check WiFi connection status
     if(WiFi.status()== WL_CONNECTED){
-      String serverPath = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + countryCode + "&APPID=" + openWeatherMapApiKey;
+      HTTPClient http;
+
+      String serverPath = serverName + "?Mode=W";
       
-      jsonBuffer = httpGETRequest(serverPath.c_str());
-      Serial.println(jsonBuffer);
-      JSONVar myObject = JSON.parse(jsonBuffer);
-  
-      // JSON.typeof(jsonVar) can be used to get the type of the var
-      if (JSON.typeof(myObject) == "undefined") {
-        Serial.println("Parsing input failed!");
-        return;
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverPath.c_str());
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
       }
-    
-      Serial.print("JSON object = ");
-      Serial.println(myObject);
-      Serial.print("Temperature: ");
-      Serial.println(myObject["main"]["temp"]);
-      Serial.print("Pressure: ");
-      Serial.println(myObject["main"]["pressure"]);
-      Serial.print("Humidity: ");
-      Serial.println(myObject["main"]["humidity"]);
-      Serial.print("Wind Speed: ");
-      Serial.println(myObject["wind"]["speed"]);
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
     }
     else {
       Serial.println("WiFi Disconnected");
     }
     lastTime = millis();
   }
-}
-
-String httpGETRequest(const char* serverName) {
-  WiFiClient client;
-  HTTPClient http;
-    
-  // Your Domain name with URL path or IP address with path
-  http.begin(client, serverName);
-  
-  // Send HTTP POST request
-  int httpResponseCode = http.GET();
-  
-  String payload = "{}"; 
-  
-  if (httpResponseCode>0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    payload = http.getString();
-  }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  // Free resources
-  http.end();
-
-  return payload;
 }
---
+++ Control/Readme.md
@@ -0,0 +1,12 @@
+https://randomnerdtutorials.com/esp32-websocket-server-arduino/
+Above is the websocket used to asychronise the web page, we could use it later with map.
+
+https://randomnerdtutorials.com/esp32-http-get-post-arduino/
+HTTP request part, I think send a JASON object is better;
+ESP32 HTTP GET: Value or Query in URL
+ESP32 HTTP GET: JSON Data Object or Plain Text
+ESP32 HTTP POST: URL Encoded, JSON Data Object, Plain Text
+Timer set to 10 minutes (600000)
+1 min= 60000 
+
+unit is ms
